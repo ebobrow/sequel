3ebe4897573d839884d354d0b210992a856b2e7e
Elliot Bobrow <elliotgreybobrow@gmail.com>
2022-08-05 10:24:43 -0700

    rustyline

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,10 +1,12 @@
-use std::io::{self, stdout, Cursor, Write};
+use std::io::{self, Cursor};
 
 use futures::{select, FutureExt};
+use rustyline::{Config, EditMode, Editor};
 use sequel::connection::Frame;
 use tokio::{
-    io::{stdin, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
+    sync::watch,
 };
 
 // TODO: use `Connection`?
@@ -13,10 +15,32 @@ async fn main() -> io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:3000").await.unwrap();
     let (mut rd, mut wr) = stream.split();
 
-    let mut lines_from_stdin = BufReader::new(stdin()).lines();
+    let (tx, mut rx) = watch::channel(String::new());
+    // TODO: this is pretty bad
+    let (tx2, mut rx2) = watch::channel(false);
+    tokio::spawn(async move {
+        let mut rl = Editor::<()>::with_config(
+            Config::builder()
+                .auto_add_history(true)
+                .edit_mode(EditMode::Emacs)
+                .build(),
+        )
+        .unwrap();
+
+        loop {
+            let readline = rl.readline("SQL> ").unwrap();
+            tx.send(readline).unwrap();
+            loop {
+                rx2.changed().await.unwrap();
+                if *rx2.borrow() {
+                    break;
+                }
+            }
+        }
+    });
+
     let mut buf = [0; 4096];
 
-    print_prompt()?;
     loop {
         select! {
             n = rd.read(&mut buf).fuse() => {
@@ -24,22 +48,22 @@ async fn main() -> io::Result<()> {
                 if let Ok(frame) = Frame::parse(&mut cursor) {
                     println!("{}", frame);
                 }
-                print_prompt()?;
+                tx2.send(true).unwrap();
             }
-            // TODO: https://github.com/kkawakam/rustyline
-            line = lines_from_stdin.next_line().fuse() => {
-                let line = line?.unwrap();
-                wr.write_u8(b':').await.unwrap();
-                wr.write_all(line.as_bytes()).await.unwrap();
-                wr.write_all(b"\r\n").await.unwrap();
-                wr.flush().await.unwrap();
+            res = rx.changed().fuse() => {
+                tx2.send(false).unwrap();
+                if res.is_ok() {
+                    let line = rx.borrow();
+                    wr.write_u8(b':').await.unwrap();
+                    wr.write_all(line.as_bytes()).await.unwrap();
+                    wr.write_all(b"\r\n").await.unwrap();
+                    wr.flush().await.unwrap();
+                } else {
+                    break;
+                }
             }
         }
     }
-}
 
-fn print_prompt() -> io::Result<()> {
-    print!("SQL> ");
-    stdout().flush()?;
     Ok(())
 }
